ebdc6ae283fcf5a4309f61faab8575d89c45640c
Jake Calderon <calderonjake2007@icloud.com>
2026-04-20 10:38:28 -0400

    Update bomb_configs.py

--- bomb_configs.py
+++ bomb_configs.py
@@ -1,7 +1,7 @@
 #################################
 # CSC 102 Defuse the Bomb Project
 # Configuration file
-# Team: 
+# Team: Jake C, Declan L, Chevaughn H
 #################################
 
 # constants
